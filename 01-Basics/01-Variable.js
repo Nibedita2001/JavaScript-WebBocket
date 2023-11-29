7495f2f58a97c6091ddaaa469901f13348fe5ba4
Nibedita Kerketta <152247031+Nibedita2001@users.noreply.github.com>
2023-11-29 05:30:48 +0000

    File added

---
+++ 01-Basics/01-Variable.js
@@ -0,0 +1,34 @@
+/* 
+Variables :-
+1. Variable are container for storing data.
+2. JavaScript variable can be declared in 4 ways.
+    ---> Automatically
+    ---> Using var
+    ---> Using let
+    ---> Using const
+3. The var keyword was used in all javascript code from 1995 to 2015.
+4. The let and const keyword were added to javascript in 2015.
+5. The var keyword should only be used in code written for older browser.
+6. let is blocked scope where as var is function scope.
+*/
+
+const accountID = 334;
+let accountEmail = "nita@gmail.com";
+var accountPassword = "12345"
+accountcity = "Bhubaneswar";
+let accountState;
+/*console.log("Account id = " , accountID);
+console.log("Account Email = " , accountEmail);
+console.log("Account Password = " , accountPassword);
+console.log("Account City = " , accountcity);
+console.log("Account State = " , accountState);
+*/
+console.table([accountID,accountEmail,accountPassword,accountcity,accountState]);
+
+/*console.log(accountID);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountcity);
+console.log(accountState);
+*/
+
