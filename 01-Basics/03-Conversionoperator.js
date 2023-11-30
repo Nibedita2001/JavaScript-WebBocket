0b0db1de2f9c10f31662a5bd0757f80ee7269d67
Nibedita Kerketta <152247031+Nibedita2001@users.noreply.github.com>
2023-11-30 04:56:13 +0000

    file added

---
+++ 01-Basics/03-Conversionoperator.js
@@ -0,0 +1,26 @@
+//let score = "33abc";
+//let score = null;
+// let score = "gift";
+// let score = true;
+// console.log(typeof(score));
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+
+// let score = 26;
+
+// let valueInNumber = String(score);
+// console.log(typeof(valueInNumber));
+
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+console.log(booleanIsLoggedIn);
+
+/* "gift" = true
+    "   " = false
+    1     = true
+    0     = false
+*/
