593cb0d12a4df2ceab26d6d347149fd98e987989
Nibedita Kerketta <152247031+Nibedita2001@users.noreply.github.com>
2023-11-30 05:23:27 +0000

    file added

---
+++ 01-Basics/06-Variablesummary.js
@@ -0,0 +1,14 @@
+/* In our JavaScript there are 2 types of variable is present
+    1. primitive - string, number,bigInt, symbol, null, boolean, undefined
+    2. non-primitive/reference -
+        1.array --> const cars = ["BMW","audi","Lambo"];
+        2.object ---> let myObj = {
+            name : "Nibedita",
+            age  : 22,
+            city : "Bhubaneswar",
+            key  : value
+        }; //object is a key-value pair
+        3.function ---> const myFunction = function() {
+            console.log("Hello world");
+        }
+*/
