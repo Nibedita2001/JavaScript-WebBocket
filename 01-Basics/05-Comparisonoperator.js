06d2553f304b7f75f775dad17665cfb5299b99bc
Nibedita Kerketta <152247031+Nibedita2001@users.noreply.github.com>
2023-11-30 05:15:44 +0000

    file added

--- 01-Basics/05-Comparisonoperator.js
+++ 01-Basics/05-Comparisonoperator.js
@@ -1,13 +1,17 @@
-console.log(2>1);
-console.log(2>=19);
-console.log(2<1);
-console.log(2==1);
-console.log(2!=1);
-console.log("2">1);
-console.log("02">1);
-console.log(null>0);
-console.log(null==0);
-console.log(null>=0);
+// console.log(2>1);
+// console.log(2>=19);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
 
 /* The reason is that an equality check == and comparison ><>=<= work differently.
 2. The comparison convert null to a number, treating as 0 , thats why (3) null>=0 is true and (1) null>0 is false. */
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
