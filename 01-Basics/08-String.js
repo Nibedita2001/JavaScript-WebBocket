12344a77b556f751fd5c02118b8a206a505660fd
Nibedita Kerketta <152247031+Nibedita2001@users.noreply.github.com>
2023-12-04 04:40:10 +0000

    file added

---
+++ 01-Basics/08-String.js
@@ -0,0 +1,31 @@
+// const name = "Nibedita";
+// console.log(name);
+// const count = 50;
+// console.log(name + count);
+// console.log(`My name is ${name}`);  //``backtics symbol
+
+const name = new String("Nibedita");   //through object
+// console.log(name);
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.indexOf('b'));
+// console.log(name.charAt(3));
+
+
+// const newName = name.substring(0,4);
+// console.log(newName);
+
+// const myName = "    gift    ";
+// console.log(myName);
+// console.log(myName.trim());             //trim - remove white space
+
+
+// const anotherName = name.slice(0,4);        //slice - return the selected element
+// console.log(anotherName);
+
+// const url = "www.webbocket.com";                //replace - replace the element
+// console.log(url.replace("com", "in"));
+
+const demo = "gift-is-a-good-college";              //split method
+console.log(demo.split('-'));
