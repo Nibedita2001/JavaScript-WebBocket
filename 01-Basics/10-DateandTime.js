b000296a9ecca3216f3cb90de8743bc8c0c9cd7d
Nibedita Kerketta <152247031+Nibedita2001@users.noreply.github.com>
2023-12-04 05:46:24 +0000

    file added

--- 01-Basics/10-DateandTime.js
+++ 01-Basics/10-DateandTime.js
@@ -5,10 +5,20 @@
 // console.log(myDate.toLocaleDateString());
 // console.log(typeof myDate);
 
-let myOwnDate = new Date(2023,0,23);
-let myOwnDate1 = new Date(2023,0,23,5,3);
+// let myOwnDate = new Date(2023,0,23);
+// let myOwnDate1 = new Date(2023,0,23,5,3);
 
-console.log(myOwnDate.toDateString());
-console.log(myOwnDate1.toDateString());
+// console.log(myOwnDate.toDateString());
+// console.log(myOwnDate1.toDateString());
+// console.log(myOwnDate1.toLocaleTimeString());
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay()); //0-6 //sunday-saturday
+console.log(newDate.getFullYear);
+console.log(newDate.getHours());
+console.log(newDate.getMinutes());
+console.log(newDate.getSeconds());
 
 
