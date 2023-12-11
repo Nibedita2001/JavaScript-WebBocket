aa436815b93b1ac0dc3421918767e8fafb284060
Nibedita Kerketta <152247031+Nibedita2001@users.noreply.github.com>
2023-12-11 05:06:49 +0000

    file added

---
+++ 02-Intermediate/arrowFunction.js
@@ -0,0 +1,25 @@
+/*
+Arrow Function :-
+- Arrow function is also called as fat arrow function is a features in es6 in JavaScript to write consize function in js
+*/
+
+// function timesTwo(params) {
+//     return params * 2
+// }function timesTwo(params)
+// {
+//     return params*2
+// }
+// console.log(timesTwo(14));
+
+// var timesTwo = params => params * 2
+// console.log(timesTwo(4));
+
+/* 
+dom
+event
+async
+api request
+promise
+call back
+closer
+ */
