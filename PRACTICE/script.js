0a509babf58ee1a2cc8d3787a00289416b91851b
Nibedita Kerketta <152247031+Nibedita2001@users.noreply.github.com>
2023-12-17 03:44:42 +0000

    file added

--- PRACTICE/script.js
+++ PRACTICE/script.js
@@ -1,8 +1,40 @@
-// console.log("Hello");
-// alert("Apna College");
-// console.log(window);
-// console.log("hello");
-// window.console.log("hello2");
-// window.alert("hello");
+// console.log(window.document.head);
+// console.dir(window.document.head);
+// console.log(document.body.childNodes[2]);
 
 
+// console.log(document.body);
+
+// let headings = document.getElementsByClassName("myClass");
+// console.log(headings);
+// console.dir(headings);
+
+// let cls = document.getElementsByClassName("myClass");
+// console.log(cls);
+// console.dir(cls);
+
+// let head = document.getElementById("myId");
+// console.log(head);
+
+// let parahs = document.getElementsByTagName("p");
+// console.log(parahs);
+// console.dir(parahs);
+
+// let elements = document.querySelector("h4");
+// console.dir(elements);
+// let elements2 = document.querySelectorAll("p");
+// console.dir(elements2);
+
+// let example = document.querySelector("h1");
+// console.dir(example);
+// let example2 = document.querySelectorAll("h1");
+// console.dir(example2);
+
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+let div = document.querySelector("div");
+console.dir(div);
+
+let heading = document.querySelector("h1");
+console.log(heading);
